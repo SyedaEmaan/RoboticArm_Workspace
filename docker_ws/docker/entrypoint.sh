4e7729196f6937835ac3d54f3504d99b76fb07b5
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2026-03-12 15:24:31 +0500

    Adding PID gains to my ros2_controllers.yaml file

--- docker_ws/docker/entrypoint.sh
+++ docker_ws/docker/entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 source /opt/ros/humble/setup.bash 
 source /usr/share/gazebo-11/setup.sh
 
+# Source EtherCAT driver workspace
+if [ -f /home/rosdev/ethercat_ws/install/setup.bash ]; then
+    source /home/rosdev/ethercat_ws/install/setup.bash
+fi
+
 if [ -f /home/rosdev/arm_ws/install/setup.bash ]; then 
 	source /home/rosdev/arm_ws/install/setup.bash
 fi
